eb0304d71fde1e3b6a3b78952a8e988f9c30c1b2
Rexream <murataslann4934@gmail.com>
2026-08-11 14:08:39 +0300

    Added first design and functions. Added Sign-in, Sign-up and Log-out functions. Added Homepage and Log-in pages.

---
+++ lib/pages/guest_book.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+
+import '../src/widgets.dart';
+
+class GuestBook extends StatefulWidget {
+  const GuestBook({super.key, required this.addMessage});
+
+  final FutureOr<void> Function(String message) addMessage;
+
+  @override
+  State<GuestBook> createState() => _GuestBookState();
+}
+
+class _GuestBookState extends State<GuestBook> {
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+
+      child: Form(
+        key: _formKey,
+
+        child: Row(
+          children: [
+            Expanded(
+              child: TextFormField(
+                controller: _controller,
+
+                decoration: const InputDecoration(hintText: 'Mesaj yaz'),
+
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Mesaj yeri boş bırakılamaz.';
+                  }
+
+                  return null;
+                },
+              ),
+            ),
+
+            const SizedBox(width: 8),
+
+            StyledButton(
+              onPressed: () async {
+                if (_formKey.currentState!.validate()) {
+                  await widget.addMessage(_controller.text);
+
+                  _controller.clear();
+                }
+              },
+
+              child: Row(
+                children: [
+                  Icon(Icons.send),
+                  SizedBox(width: 4),
+                  Text("GÖNDER"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/homepage.dart
@@ -0,0 +1,67 @@
+import 'package:firebase_deneme/pages/guest_book.dart';
+import 'package:flutter/material.dart';
+
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, PhoneAuthProvider;
+
+import 'package:provider/provider.dart';
+
+import '../app_state.dart';
+import '../src/authentication.dart';
+import '../src/widgets.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("FIREBASE ÖGRENME")),
+
+      body: ListView(
+        children: [
+          Image.asset('assets/codelab.png'),
+          const SizedBox(height: 8),
+          const IconAndDetail(Icons.calendar_today, '21.08'),
+          const IconAndDetail(Icons.location_city, 'İstanbul'),
+
+          Consumer<ApplicationState>(
+            builder: (context, appstate, _) => AuthFunc(
+              loggedIn: appstate.loggedin,
+              signOut: () {
+                FirebaseAuth.instance.signOut();
+              },
+            ),
+          ),
+
+          const Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+
+          const Header("Bugün ne yiyeceğiz"),
+          const Paragraph("Pasta yada dondurma"),
+
+          const Divider(
+            height: 8,
+            thickness: 1,
+            indent: 8,
+            endIndent: 8,
+            color: Colors.grey,
+          ),
+
+          const Header("Sohbet"),
+          GuestBook(addMessage: (mesaage) => print(mesaage)),
+        ],
+      ),
+    );
+  }
+}
